5d79a46fbd5a938d82fccde79185c85595b42b80
40huo <git@40huo.cn>
2017-09-15 17:41:46 +0800

    合并JS

--- static/offer/js/offer.js
+++ static/offer/js/offer.js
@@ -1,3 +1,13 @@
 $('#delete-btn').click(function () {
-    return confirm('ssss');
+    return confirm('确认删除？');
+});
+
+$(".form_datetime").datepicker({
+    format: 'yyyy-mm-dd',
+    language: 'zh-CN',
+    todayBtn: 'linked',
+    clearBtn: true,
+    todayHighlight: true,
+    autoclose: true,
+    orientation: "bottom auto"
 });
