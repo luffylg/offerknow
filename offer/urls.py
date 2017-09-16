d51df129d25359745ce94f52c80dcff927b24058
40huo <git@40huo.cn>
2017-09-16 12:11:03 +0800

    换用semantic ui

--- offer/urls.py
+++ offer/urls.py
@@ -7,7 +7,7 @@ from offer import views
 app_name = 'offer'
 urlpatterns = [
     url(r'^$', view=views.index, name='index'),
-    url(r'^add-offer/$', view=views.add_offer, name='add_offer'),
+    url(r'^add/$', view=views.add_offer, name='add_offer'),
     url(r'^edit/(?P<offer_id>\d+)/$', view=views.update_offer, name='edit_offer'),
     url(r'^delete/(?P<offer_id>\d+)/$', view=views.delete_offer, name='delete_offer'),
 ]
